f7547b10e326cbb6edbd2ecc07ecf32780b0b721
Alex Morton <mortonar01@gmail.com>
2026-04-15 15:59:21 -0400

    [Bare Bones] - Testing
    * Set up test framework for kernel
    * Add isa-debug-exit QEMU device support so test runner can exit cleanly
    * Basic serial driver support to enable printing to host console
    * Factored out a library to share functionality

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,28 +4,22 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::println;
 use core::panic::PanicInfo;
+use blog_os::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
