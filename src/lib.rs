6055ba4e10e95605bd2dc95cf3e9eafba546c8a2
Alex Morton <mortonar01@gmail.com>
2026-04-23 13:58:54 -0400

    [Interrupts] Double Faults

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
@@ -21,6 +22,7 @@ pub extern "C" fn _start() -> ! {
 }
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
 }
 
